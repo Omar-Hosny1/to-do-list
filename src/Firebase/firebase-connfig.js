ecd19ab4a0818bcac331a3df7a0fb80286790beb
Omar Hosny <honsyomar022@gmail.com>
2022-08-03 13:43:41 +0200

    All Dependencies Added

---
+++ src/Firebase/firebase-connfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDeY5FC587gVdUhgxqA296nZeyTXBCuv1E",
+  authDomain: "todolist-ac82f.firebaseapp.com",
+  projectId: "todolist-ac82f",
+  storageBucket: "todolist-ac82f.appspot.com",
+  messagingSenderId: "387310757668",
+  appId: "1:387310757668:web:bb9c4edb32112e4fa6f5b1",
+};
+
+const app = initializeApp(firebaseConfig);
