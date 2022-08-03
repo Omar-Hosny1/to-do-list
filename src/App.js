5cebc79eeee2a6d196089ae571887eaf53c4de8c
Omar Hosny <honsyomar022@gmail.com>
2022-08-03 14:28:04 +0200

    Routes Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import {} from "react-dom";
+import { Routes, Route } from "react-router-dom";
 import GoogleAuth from "./components/Authentication/GoogleAuth";
+import AuthPage from "./pages/AuthPage";
+import MainPage from "./pages/MainPage";
 function App() {
   return (
     <div className="App">
-      <GoogleAuth />
+      <Routes>
+        <Route path="/" element={<AuthPage />} />
+        <Route path="/main-page" element={<MainPage />} />
+      </Routes>
     </div>
   );
 }
